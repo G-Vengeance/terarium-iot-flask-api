e8b875d370c97341ce7f0a5db593a3db8d1ac4c5
Gerrio Irfan Pratama <134785241+G-Vengeance@users.noreply.github.com>
2025-07-31 23:20:34 +0700

    Fix DB URI and add PostgreSQL driver

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ app = Flask(__name__)
 
 # Konfigurasi Database untuk Heroku (PostgreSQL) dan Lokal (SQLite)
 # Heroku akan menyediakan variabel lingkungan DATABASE_URL
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///terarium_data.db')
+uri = os.getenv("DATABASE_URL", "sqlite:///terarium_data.db")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
